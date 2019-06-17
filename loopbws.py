a7ab0dda932f84bdd808ffeebf4435fc9e4e1ac9
Joe <Johari19@users.noreply.github.com>
2019-06-17 17:25:56 -0400

    Update loopbws.py

--- loopbws.py
+++ loopbws.py
@@ -9,7 +9,6 @@ print("Script made by: https://github.com/Johari19\n")
 print("This is a looping variants of the original scraper!")
 print("Use this for things like stocks, time and etc.")
 print("Sometimes this does not work as the website has a static value for the element\n")
-print("NOTE: TOO MANY REQUESTS IN A GIVEN TIME PERIOD MAY LEAD TO BEING IP BANNED!")
 # Variables
 url = str(input("Enter url of the site you would like to scrape (USE: https://www.websitename.com): "))
 if not url:
@@ -52,4 +51,4 @@ while True:
     for element in html.select(foo):
         eltext = element.get_text()
         print(eltext, end="\r")
-    time.sleep(1.25)
+    time.sleep(1.25) # <---- DO NOT REMOVE! IF REMOVED, USER MAY RISK IP BAN FROM WEBSITE!
