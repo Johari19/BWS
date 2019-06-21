85d4fb99bb8f8707f727d1c0e38f221c17ed0a60
Joe <Johari19@users.noreply.github.com>
2019-06-21 15:07:54 -0400

    change datetime to just time

--- loopbws.py
+++ loopbws.py
@@ -50,7 +50,7 @@ f.close()
 print("I'm saving all the values in an easily formatable .csv file! Check value.csv")
 print("\nI found:")
 while True:
-    timestamp = str(datetime.now())
+    timestamp = str(datetime.now().strftime('%H:%M:%S'))
     raw_html = get_URL(url)
     html = BeautifulSoup(raw_html, 'html.parser')
     for element in html.select(foo):
@@ -60,5 +60,4 @@ while True:
         writer.writerow([timestamp, eltext])
         f.close()
         print('[' + timestamp + '] ' + eltext, end="\r")
-
     time.sleep(1.25) # <---- DO NOT REMOVE! IF REMOVED, USER MAY RISK IP BAN FROM WEBSITE!
